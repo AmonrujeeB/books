7e3f1289bc68f0aa6c00ba51e3286e4b516324f5
“Amonrujee <61160131@go.buu.ac.th>
2021-03-04 19:42:41 +0700

    update POST /books to store book data into MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 const app = express()
 
 app.use(express.json())
 let books = []
 
-app.get('/books/:id', async(req, res) => {
+const url = 'mongodb+srv://superadmin:31122541ying@cluster0.g0htt.mongodb.net/books?retryWrites=true&w=majority'
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
+let db, moviesCollection
+
+async function connect() {
+    await client.connect()
+    db = client.db('books')
+    booksCollection = db.collection('books')
+}
+connect()
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------
+app.get('/books', async(req, res) => {
+    const cursor = await booksCollection.find({})
+    const result = await cursor.toArray()
     res.status(200).json(books)
 })
 
+app.get('/books/:id', async(req, res) => {
+    //input
+    let id = req.params.id
+        // let movie = {}
+
+    //process
+    const book = await booksCollection.findOne({ _id: ObjectId(id) })
+
+    //output
+    res.status(200).json(book)
+})
 
 
 app.post('/books', (req, res) => {
@@ -32,5 +58,14 @@ app.post('/books', (req, res) => {
 
     res.status(201).json(bookID)
 })
+
+const result = await booksCollection.insertOne(newBooks)
+    // movies.push(newMovie)
+    //n-1
+    // movieID = movies.length -1
+bookID = result.insertedId
+
+//output 
+res.status(201).json(bookID)
 const port = 3000
 app.listen(port, () => console.log(`Server started at ${port}`))
